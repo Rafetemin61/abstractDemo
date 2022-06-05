591737afaf966154bb09593bf0841446db8433c6
Rafet Emin Alpaslan <34487796+Rafetemin61@users.noreply.github.com>
2022-06-05 17:54:44 +0300

    abstractDemo
    Abstract Class work

---
+++ abstractDemo/src/abstractDemo/SqlDatabaseManager.java
@@ -0,0 +1,12 @@
+package abstractDemo;
+
+public class SqlDatabaseManager extends BaseDatabaseManager{
+
+	
+	public void getData() {
+		System.out.println("Veri getirildi : Sql Server");
+		
+		
+	}
+
+}
